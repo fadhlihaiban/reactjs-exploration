bfd73a24ed3db509341ee721d4aa434e5e670844
Fadhli Haiban <fadhlihaibank123@gmail.com>
2026-01-16 10:17:17 +0700

    Tampil dan tambah list

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,45 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  //1. State menyimpan list
+  const [items, setItems] = useState(['Belajar React','Beli Kopi']);
+  
+  //2. State menangkap input dari form (seperti $_POST)
+  const [inputBaru, setInputBaru] = useState('');
+
+  const tambahData = () => {
+    if (inputBaru.trim() !== ""){
+      setItems([...items, inputBaru]);
+      setInputBaru(''); // Kosongkan input setelah tambah
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+      <h2>Daftar Kegiatan</h2>
+
+      {/* Input Form */}
+      <input
+        type="text"
+        value={inputBaru}
+        onChange={(e) => setInputBaru(e.target.value)}
+        placeholder="Ketik sesuatu..."
+      />
+      <button onClick={tambahData}>Tambah</button>
+
+      {/* Menampilkan Data*/}
+      <ul>
+        {items.map((item, index) => (
+          <li>
+            {item}
+            <button onClick={() => setItems(items.filter((_, i) => i !== index))}>
+              Hapus
+            </button>
+          </li>
+        ))}
+      </ul>
+
+    </div>
   )
 }
 
