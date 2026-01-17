596f3de7650ca9d34ea4a114c0084552acfd0564
Fadhli Haiban <fadhlihaibank123@gmail.com>
2026-01-17 13:47:55 +0700

    004_import_export_file_lain

---
+++ dokumentasi/004_import_export_file_lain/src/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header style={{ backgroundColor: '#282c34', padding: '10px', color: 'white' }}>
+            <h1>Dashboard</h1>
+        </header>
+    );
+}
+
+export default Header; // ini membuatnya bisa di-include file lain
